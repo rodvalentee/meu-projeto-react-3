684cc031bddc96ee93c1947a8d6f7de2d79ab845
rodvalentee <rodrigovalente.ti@gmail.com>
2023-05-28 17:09:29 -0300

    fim

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,48 @@
-import { useState } from "react";
-import MinhaLista from "./components/MinhaLista";
+import { useEffect, useState } from "react";
 
+
+const minhaLista = [
+    {id:'1', value: "Fruta"},
+    {id:'2', value: "Legume"},
+    {id:'3', value: "Carne"},
+]
 export default function App() {
 
-  
-  
+  const [produtos, setProdutos] = useState(minhaLista)
+  const [pesquisa, setPesquisa] = useState('')
 
+  useEffect(
+    () => {
+      if(pesquisa){
+        const  novaLista = minhaLista.filter((item) => {
+          return item.value.toLocaleLowerCase().includes(pesquisa.toLocaleLowerCase())
+        })
+        setProdutos(novaLista)
+      }
+      else{
+        setProdutos(minhaLista)
+      }
+    }
+  , [pesquisa])
+  
+  
   return (
-      
-      <div>
-       
-        <h1></h1>
-        <MinhaLista />
-      </div>
+    <div>
+      <h1>Efeitos colaterais</h1>
+      <input 
+      value={pesquisa}
+      onChange={(e) => setPesquisa(e.target.value)}
+      placeholder="Pesquise aqui"></input>
+      {produtos.map((item) => {
+        return(
+          <div key = {item.id}>
+            <h4>{item.value}</h4>
+          </div>
+
+        )
+      })}
+    </div>
+
   )
 }
 
