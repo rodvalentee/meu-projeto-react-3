19468a599d8767b301335c1199f36277ccdb4a71
rodvalentee <rodrigovalente.ti@gmail.com>
2023-05-28 16:52:28 -0300

    Listas

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import MinhaLista from "./components/MinhaLista";
+
+export default function App() {
+
+  
+  
+
+  return (
+      
+      <div>
+       
+        <h1></h1>
+        <MinhaLista />
+      </div>
+  )
+}
+
+
